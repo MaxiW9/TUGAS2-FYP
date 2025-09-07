7d96580286863a78ccda1a4e6d7bcd8f135b1e4b
Willbert <maximillianwillbert@gmail.com>
2025-09-07 19:05:07 +0700

    commit on main

---
+++ FYP_TugasASG2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, I'm Maximillian Willbertssen\n");
+    printf("my favorite programming language is C\n");
+    printf("Because it is the first programming language I learned\n");
+    return 0;
+}
